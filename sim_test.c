1f37be7f9b8e2c6ed085c11b9d479eeea458d8a4
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2023-09-06 17:58:34 +0100

    finished Gnuplot image test

--- sim_test.c
+++ sim_test.c
@@ -13,20 +13,34 @@ int sim_test(int argc, char* argv[])
 	CLAP_CARG(nrows,   int,     20,           "number of rows");
 	puts("---------------------------------------------------------------------------------------\n");
 
-	FILE* data = gp_dopen("test",NULL);
+	// The data
+
+	const size_t npix = (size_t)ncols*(size_t)nrows;
+	float* const dat = malloc(npix*sizeof(float));
+	float* pdat = dat;
 	for (int i=0;i<nrows;++i) {
+		const float y = (float)i/(float)nrows;
 		for (int j=0;j<ncols;++j) {
-			const float p = sinf((float)i)*cosf((float)j);
-			fwrite(&p,sizeof(float),1,data);
+			const float x = (float)j/(float)ncols;
+			*pdat++ = x*cosf(x)*sinf(1.0f/(0.1f+y/10.0f));
 		}
 	}
+
+	// Write data to file (bianry)
+	FILE* data = gp_dopen("test",NULL);
+	fwrite(dat,sizeof(float),npix,data);
 	fclose(data);
+
+	free(dat);
+
+	// Plot as image
 
 	FILE* gp = gp_fopen("test",NULL,NULL);
 	fprintf(gp,"data = \"test.dat\"\n");
+	fprintf(gp,"set size ratio -1\n");
 	fprintf(gp,"set xr [-0.5:%g]\n",(double)ncols-0.5);
 	fprintf(gp,"set yr [-0.5:%g]\n",(double)nrows-0.5);
-	fprintf(gp,"plot data binary array=(%d,%d) with image\n",ncols,nrows);
+	fprintf(gp,"plot data binary array=(%d,%d) with image not\n",ncols,nrows);
 	fclose(gp);
 
 	gp_fplot("test",NULL,NULL);
