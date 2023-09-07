7e2b484fbc0ea98a2919f89c8ba6288bfdfae146
lcbarnett <l.c.barnett@sussex.ac.uk>
2023-09-07 12:55:22 +0100

    Gnuplot image test proof of principle

--- sim_test.c
+++ sim_test.c
@@ -11,6 +11,7 @@ int sim_test(int argc, char* argv[])
 	puts("\n---------------------------------------------------------------------------------------");
 	CLAP_CARG(ncols,   int,     100,          "number of columns");
 	CLAP_CARG(nrows,   int,     200,          "number of rows");
+	CLAP_CARG(gpipe,   int,     0,            "pipe to Gnuplot?");
 	puts("---------------------------------------------------------------------------------------\n");
 
 	// The data
@@ -26,25 +27,43 @@ int sim_test(int argc, char* argv[])
 		}
 	}
 
-	// Write data to file (binary)
+	// Note: for Gnuplot binary array with image, default graph 0,0 is bottom left; flip=y for top left
 
-	FILE* data = gp_dopen("test",NULL);
-	fwrite(dat,sizeof(float),npix,data);
-	fclose(data);
+	if (gpipe) {
+		printf("Plotting to Gnuplot pipe\n");
+
+		// Pipe data to Gnuplot (binary) and plot as image
+
+		FILE* gp = gp_popen(NULL,NULL);
+		fprintf(gp,"set size ratio -1\n");
+		fprintf(gp,"set xr [-0.5:%g]\n",(double)ncols-0.5);
+		fprintf(gp,"set yr [-0.5:%g]\n",(double)nrows-0.5);
+		fprintf(gp,"plot '-' binary array=(%d,%d) flip=y with image not\n",ncols,nrows);
+		fwrite(dat,sizeof(float),npix,gp);
+		if (pclose(gp) == EOF) PEEXIT("failed to close pipe to Gnuplot\n");
+	}
+	else {
+		printf("Plotting using Gnuplot command file\n");
+
+		// Write data to file (binary)
+
+		FILE* data = gp_dopen("test",NULL);
+		fwrite(dat,sizeof(float),npix,data);
+		if (fclose(data) == EOF) PEEXIT("failed to close Gnuplot data file\n");
+
+		// Plot data as image
+
+		FILE* gp = gp_fopen("test",NULL,NULL);
+		fprintf(gp,"data = \"test.dat\"\n");
+		fprintf(gp,"set size ratio -1\n");
+		fprintf(gp,"set xr [-0.5:%g]\n",(double)ncols-0.5);
+		fprintf(gp,"set yr [-0.5:%g]\n",(double)nrows-0.5);
+		fprintf(gp,"plot data binary array=(%d,%d) flip=y with image not\n",ncols,nrows);
+		if (fclose(gp) == EOF) PEEXIT("failed to close Gnuplot command file\n");
+		gp_fplot("test",NULL,NULL);
+	}
 
 	free(dat);
-
-	// Plot as image (note: default 0,0 is bottom left; flip=y for top left)
-
-	FILE* gp = gp_fopen("test",NULL,NULL);
-	fprintf(gp,"data = \"test.dat\"\n");
-	fprintf(gp,"set size ratio -1\n");
-	fprintf(gp,"set xr [-0.5:%g]\n",(double)ncols-0.5);
-	fprintf(gp,"set yr [-0.5:%g]\n",(double)nrows-0.5);
-	fprintf(gp,"plot data binary array=(%d,%d) flip=y with image not\n",ncols,nrows);
-	fclose(gp);
-
-	gp_fplot("test",NULL,NULL);
 
 	return EXIT_SUCCESS;
 }
