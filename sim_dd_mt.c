32725ff1cc589b039fc7306f48b3ff1c018ad11f
Lionel Barnett <l.c.barnett@sussex.ac.uk>
2023-11-05 17:32:54 +0000

    Refactored sim_dd_mt.c to save thread computations and then write results to file

--- sim_dd_mt.c
+++ sim_dd_mt.c
@@ -3,12 +3,12 @@
 #include "clap.h"
 #include "rtab.h"
 
-#define ofnmaxlen 1000
-
 typedef struct {
-	rtl_t* rule;
-	rtl_t* filt;
-	char*  ofname;
+	rtl_t*  rule;
+	rtl_t*  filt;
+	double* Hr;
+	double* Hf;
+	double* DD;
 } tfarg_t;
 
 typedef struct {
@@ -19,85 +19,10 @@ typedef struct {
 	int tmmax;
 	int tiff;
 	int tlag;
-	tfarg_t* tfarg;
+	tfarg_t* tfargs;
 } targ_t;
 
-void* compfun(void* arg)
-{
-	const pthread_t tpid = pthread_self();
-	const targ_t* const targ = (targ_t*)arg;
-	const int tnum  = targ->tnum;
-	const int nfint = targ->nfint;
-
-	printf("thread %2d (%zu) : %d filters : STARTED\n",tnum+1,tpid,nfint);
-	fflush(stdout);
-
-	const int emmax = targ->emmax;
-	const int eiff  = targ->eiff;
-	const int tmmax = targ->tmmax;
-	const int tiff  = targ->tiff;
-	const int tlag  = targ->tlag;
-	const int hlen  = (emmax > tmmax ? emmax : tmmax)+1;
-
-	const tfarg_t* const tfarg = targ->tfarg;
-
-	double Hr[hlen];
-	double Hf[hlen];
-	double DD[hlen];
-
-	for (int i=0; i<nfint; ++i) {
-
-		const int           rsize  = tfarg[i].rule->size;
-		const int           fsize  = tfarg[i].filt->size;
-		const word_t* const rtab   = tfarg[i].rule->tab;
-		const word_t* const ftab   = tfarg[i].filt->tab;
-		const char*   const ofname = tfarg[i].ofname;
-
-		const int rfsize = rsize > fsize ? rsize : fsize;
-
-		for (int m=0; m<hlen; ++m) Hr[m] = NAN;
-		for (int m=0; m<hlen; ++m) Hf[m] = NAN;
-		for (int m=0; m<hlen; ++m) DD[m] = NAN;
-		for (int m=rsize; m<=emmax; ++m) {
-			Hr[m] = rt_entro(rsize,rtab,m,eiff)/(double)m;
-		}
-		for (int m=fsize; m<=emmax; ++m) {
-			Hf[m] = rt_entro(fsize,ftab,m,eiff)/(double)m;
-		}
-		for (int m=rfsize; m<=tmmax; ++m) {
-			DD[m] = rt_trent1(rsize,rtab,fsize,ftab,m,tiff,tlag)/(double)m;
-		}
-
-		FILE* const dfs = fopen(ofname,"w");
-		PASSERT(dfs != NULL,"thread %2d : failed to open output file \"%s\"\n",tnum,ofname);
-		for (int m=0; m<hlen; ++m) fprintf(dfs,"%4d\t%8.6f\t%8.6f\t%8.6f\n",m,Hr[m],Hf[m],DD[m]);
-		if (fclose(dfs) == -1) PEEXIT("thread %2d : failed to close output file\n",tnum);
-
-		flockfile(stdout); // prevent another thread butting in!
-		printf("\tthread %2d : filter %2d of %2d : rule id = ",tnum+1,i+1,nfint);
-		rt_print_id(rsize,rtab);
-		printf(", filter id = ");
-		rt_print_id(fsize,ftab);
-		printf(" : rule entropy ≈ %8.6f, filter entropy ≈ %8.6f, DD ≈ %8.6f : written \"%s\"\n",Hr[emmax],Hf[emmax],DD[tmmax],ofname);
-		fflush(stdout);
-		funlockfile(stdout);
-	}
-
-	printf("thread %2d : FINISHED\n",tnum+1);
-	fflush(stdout);
-
-	pthread_exit(NULL);
-}
-
-size_t set_ofname(char* const ofname, const rtl_t* const r, const rtl_t* const f, const char* const odir)
-{
-	snprintf(ofname,ofnmaxlen,"%s/cadd_",odir);
-	rt_sprint_id(r->size,r->tab,ofnmaxlen,ofname);
-	strncat(ofname,"_",1);
-	rt_sprint_id(f->size,f->tab,ofnmaxlen,ofname);
-	strncat(ofname,".dat",4);
-	return strlen(ofname);
-}
+void* compfun(void* arg);
 
 int sim_dd_mt(int argc, char* argv[])
 {
@@ -144,43 +69,43 @@ int sim_dd_mt(int argc, char* argv[])
 
 	// thread-independent parameters
 
-	targ_t targ[nthreads];
+	targ_t targs[nthreads];
 	for (int tnum=0; tnum<nthreads; ++tnum) {
-		targ[tnum].emmax = emmax;
-		targ[tnum].eiff  = eiff;
-		targ[tnum].tmmax = tmmax;
-		targ[tnum].tiff  = tiff;
-		targ[tnum].tlag  = tlag;
-		targ[tnum].tfarg = malloc((size_t)nfpert*sizeof(tfarg_t));
-		TEST_ALLOC(targ[tnum].tfarg);
+		targs[tnum].emmax  = emmax;
+		targs[tnum].eiff   = eiff;
+		targs[tnum].tmmax  = tmmax;
+		targs[tnum].tiff   = tiff;
+		targs[tnum].tlag   = tlag;
+		targs[tnum].tfargs = malloc((size_t)nfpert*sizeof(tfarg_t));
+		TEST_ALLOC(targs[tnum].tfargs);
 	}
 
 	// loop through rules/filters, setting up thread-dependent parameters
 
-	char ofname[ofnmaxlen+1];
-
+	const int hlen = (emmax > tmmax ? emmax : tmmax)+1;
 	int tnum  = 0;
 	int nfint = 0;
 	for (rtl_t* r = rtl_init(rule); r != NULL; r = r->next) {
 		for (rtl_t* f = r->filt; f != NULL; f = f->next) {
-			tfarg_t* const tfarg =  &targ[tnum].tfarg[nfint];
+			targ_t*  const targ  = &targs[tnum];
+			tfarg_t* const tfarg = &targ->tfargs[nfint];
 			tfarg->rule = r;
 			tfarg->filt = f;
-			const size_t ofnlen = set_ofname(ofname,r,f,odir);
-			tfarg->ofname = malloc(ofnlen+1);
-			TEST_ALLOC(tfarg->ofname);
-			strncpy(tfarg->ofname,ofname,ofnlen+1);
+			tfarg->Hr = malloc((size_t)hlen*sizeof(double));
+			tfarg->Hf = malloc((size_t)hlen*sizeof(double));
+			tfarg->DD = malloc((size_t)hlen*sizeof(double));
 			if (++nfint == nfpert) {
-				targ[tnum].tnum  = tnum;
-				targ[tnum].nfint = nfint;
+				targ->tnum  = tnum;
+				targ->nfint = nfint;
 				++tnum;
 				nfint = 0;
 			}
 		}
 	}
 	if (nfint > 0) {
-		targ[tnum].tnum  = tnum;
-		targ[tnum].nfint = nfint;
+		targ_t* const targ = &targs[tnum];
+		targ->tnum  = tnum;
+		targ->nfint = nfint;
 	}
 
 	// set up computational threads as joinable
@@ -193,7 +118,7 @@ int sim_dd_mt(int argc, char* argv[])
 	// create threads
 
 	for (int tnum=0; tnum<nthreads; ++tnum) {
-		const int tres = pthread_create(&threads[tnum],&attr,compfun,(void*)&targ[tnum]);
+		const int tres = pthread_create(&threads[tnum],&attr,compfun,(void*)&targs[tnum]);
 		PASSERT(tres == 0,"unable to create thread %d",tnum+1)
 	}
 
@@ -205,14 +130,99 @@ int sim_dd_mt(int argc, char* argv[])
 		PASSERT(tres == 0,"unable to join thread %d",tnum+1)
 	}
 
+	// write out results
+
+	const size_t ofnlen = strlen(odir)+10;
+	char ofname[ofnlen];
+	snprintf(ofname,ofnlen,"%s/cadd.dat",odir);
+	printf("\nWriting results to \"%s\"... ",ofname);
+	fflush(stdout);
+	FILE* const dfs = fopen(ofname,"w");
+	PASSERT(dfs != NULL,"Failed to open output file \"%s\"\n",ofname);
+	for (int tnum=0; tnum<nthreads; ++tnum) {
+		targ_t* const targ = &targs[tnum];
+		for (int i=0; i<targ->nfint; ++i) {
+			tfarg_t* const tfarg = &targ->tfargs[i];
+			fprintf(dfs,"# rule id = ");
+			rt_fprint_id(tfarg->rule->size,tfarg->rule->tab,dfs);
+			fprintf(dfs,", filter id = ");
+			rt_fprint_id(tfarg->filt->size,tfarg->filt->tab,dfs);
+			fputc('\n',dfs);
+			for (int m=0; m<hlen; ++m) fprintf(dfs,"%4d\t%8.6f\t%8.6f\t%8.6f\n",m,tfarg->Hr[m],tfarg->Hf[m],tfarg->DD[m]);
+			fputs("\n",dfs);
+		}
+	}
+	if (fclose(dfs) == -1) PEEXIT("Failed to close output file \"%s\"\n",ofname);
+	puts("done");
+
 	// clean up
 
 	for (int tnum=0; tnum<nthreads; ++tnum) {
-		for (int i=0; i<targ[tnum].nfint; ++i) free(targ[tnum].tfarg[i].ofname);
-		free(targ[tnum].tfarg);
+		targ_t* const targ = &targs[tnum];
+		for (int i=0; i<targ->nfint; ++i) {
+			tfarg_t* const tfarg = &targ->tfargs[i];
+			free(tfarg->DD);
+			free(tfarg->Hf);
+			free(tfarg->Hr);
+		}
+		free(targ->tfargs);
 	}
 
 	rtl_free(rule);
 
 	return EXIT_SUCCESS;
+}
+
+void* compfun(void* arg)
+{
+	const pthread_t tpid = pthread_self();
+	const targ_t* const targs = (targ_t*)arg;
+	const int tnum  = targs->tnum;
+	const int nfint = targs->nfint;
+
+	printf("thread %2d (%zu) : %d filters : STARTED\n",tnum+1,tpid,nfint);
+	fflush(stdout);
+
+	const int emmax = targs->emmax;
+	const int eiff  = targs->eiff;
+	const int tmmax = targs->tmmax;
+	const int tiff  = targs->tiff;
+	const int tlag  = targs->tlag;
+	const int hlen  = (emmax > tmmax ? emmax : tmmax)+1;
+
+	const tfarg_t* const tfargs = targs->tfargs;
+
+	for (int i=0; i<nfint; ++i) {
+
+		const int           rsize = tfargs[i].rule->size;
+		const int           fsize = tfargs[i].filt->size;
+		const word_t* const rtab  = tfargs[i].rule->tab;
+		const word_t* const ftab  = tfargs[i].filt->tab;
+		double*       const Hr    = tfargs[i].Hr;
+		double*       const Hf    = tfargs[i].Hf;
+		double*       const DD    = tfargs[i].DD;
+
+		const int rfsize = rsize > fsize ? rsize : fsize;
+
+		for (int m=0; m<hlen; ++m) Hr[m] = NAN;
+		for (int m=0; m<hlen; ++m) Hf[m] = NAN;
+		for (int m=0; m<hlen; ++m) DD[m] = NAN;
+		for (int m=rsize;  m<=emmax; ++m) Hr[m] = rt_entro(rsize,rtab,m,eiff)/(double)m;
+		for (int m=fsize;  m<=emmax; ++m) Hf[m] = rt_entro(fsize,ftab,m,eiff)/(double)m;
+		for (int m=rfsize; m<=tmmax; ++m) DD[m] = rt_trent1(rsize,rtab,fsize,ftab,m,tiff,tlag)/(double)m;
+
+		flockfile(stdout); // prevent another thread butting in!
+		printf("\tthread %2d : filter %2d of %2d : rule id = ",tnum+1,i+1,nfint);
+		rt_print_id(rsize,rtab);
+		printf(", filter id = ");
+		rt_print_id(fsize,ftab);
+		printf(" : rule entropy ≈ %8.6f, filter entropy ≈ %8.6f, DD ≈ %8.6f\n",Hr[emmax],Hf[emmax],DD[tmmax]);
+		fflush(stdout);
+		funlockfile(stdout);
+	}
+
+	printf("thread %2d : FINISHED\n",tnum+1);
+	fflush(stdout);
+
+	pthread_exit(NULL);
 }
