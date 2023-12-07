efc6ee633b730a95b876b95f880cb64cbeade71e
lcbarnett <l.c.barnett@sussex.ac.uk>
2023-12-07 17:12:37 +0000

    Minor cosmetics

--- utils.h
+++ utils.h
@@ -202,7 +202,6 @@ void  gp_pclose(FILE* const gpp);
 void  gp_fplot(const char* const gpname, const char* const gpdir);
 void  gp_setterm(FILE* const gp, const char* const gpterm, const char* const gptitle, const int xsize, const int ysize);
 
-
 // WARNING: if 'inplace' is set, x is unusable after calling (but should still be freed if allocated on heap)!
 void  gp_binary_write(FILE* const gpp, const size_t n, const double* const x, const int inplace);
 
